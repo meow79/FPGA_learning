2ce65cecb9e770b684594a010794e35f7b0a23a2
Timofeev Matvey <nomeow228@gmail.com>
2026-03-22 18:47:04 +0300

    test: move Makefile to root and make tests logs more clear
    Makefile was moved to root directory so it can be easily used from any test of any directory

--- src/02-combination-logic/exercises/build_and_test_all.sh
+++ src/02-combination-logic/exercises/build_and_test_all.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 error=0
+MAKE_GENERAL_OPS="--silent --file=../../Makefile"
 
-make SOURCES="implication.sv testbenches/implication_tb.sv" TOPMODULE=implication_tb || error=1
-make SOURCES="neg_implication.sv testbenches/neg_implication_tb.sv" TOPMODULE=neg_implication_tb || error=1
-make SOURCES="peirce_arrow.sv testbenches/peirce_arrow_tb.sv" TOPMODULE=peirce_arrow_tb || error=1
-make SOURCES="sheffer_stroke.sv testbenches/sheffer_stroke_tb.sv" TOPMODULE=sheffer_stroke_tb || error=1
+echo "----------------------------------------"
+echo "2 module test:"
+
+make $MAKE_GENERAL_OPS SOURCES="implication.sv testbenches/implication_tb.sv" TOPMODULE=implication_tb || error=1
+make $MAKE_GENERAL_OPS SOURCES="neg_implication.sv testbenches/neg_implication_tb.sv" TOPMODULE=neg_implication_tb || error=1
+make $MAKE_GENERAL_OPS SOURCES="peirce_arrow.sv testbenches/peirce_arrow_tb.sv" TOPMODULE=peirce_arrow_tb || error=1
+make $MAKE_GENERAL_OPS SOURCES="sheffer_stroke.sv testbenches/sheffer_stroke_tb.sv" TOPMODULE=sheffer_stroke_tb || error=1
 
 exit $error
