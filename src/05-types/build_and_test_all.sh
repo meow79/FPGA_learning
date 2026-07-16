4156a81ab0599306c815249d5ab7598d522866ee
Timofeev Matvey <nomeow228@gmail.com>
2026-07-17 02:54:45 +0300

    feat: solution for task 5.3 (handling inf, NaN in float_mult_byte module)

--- src/05-types/build_and_test_all.sh
+++ src/05-types/build_and_test_all.sh
@@ -7,5 +7,6 @@ echo "----------------------------------------"
 echo "5 module test:"
 
 make $MAKE_GENERAL_OPS SOURCES="01_byte_mult_double/byte_mult_double.sv 01_byte_mult_double/byte_mult_double_tb.sv" TOPMODULE=byte_mult_double_tb || error=1
+make $MAKE_GENERAL_OPS SOURCES="03_float_mult_byte_extended/float_mult_byte.sv 03_float_mult_byte_extended/float_mult_byte_tb.sv" TOPMODULE=float_mult_byte_tb || error=1
 
 exit $error
