6502355c4224df14ecb24dc86553315f21db8a14
Timofeev Matvey <nomeow228@gmail.com>
2026-07-18 14:26:26 +0300

    feat: add solution for task 5.4 (scalar product of float & byte vectors)

--- src/05-types/build_and_test_all.sh
+++ src/05-types/build_and_test_all.sh
@@ -8,5 +8,6 @@ echo "5 module test:"
 
 make $MAKE_GENERAL_OPS SOURCES="01_byte_mult_double/byte_mult_double.sv 01_byte_mult_double/byte_mult_double_tb.sv" TOPMODULE=byte_mult_double_tb || error=1
 make $MAKE_GENERAL_OPS SOURCES="03_float_mult_byte_extended/float_mult_byte.sv 03_float_mult_byte_extended/float_mult_byte_tb.sv" TOPMODULE=float_mult_byte_tb || error=1
+make $MAKE_GENERAL_OPS SOURCES="04_scalar_product/scalar_product_float_to_byte.sv 04_scalar_product/scalar_product_float_to_byte_tb.sv 03_float_mult_byte_extended/float_mult_byte.sv" TOPMODULE=scalar_product_float_to_byte_tb || error=1
 
 exit $error
