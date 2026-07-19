8196092bda15c53414c1d3f63fec881ffc951682
Timofeev Matvey <nomeow228@gmail.com>
2026-07-19 23:28:58 +0300

    fix: make outputs registered in task 6.3

--- src/06-sequential-logic/03_zero_one_counter/zero_one_counter.sv
+++ src/06-sequential-logic/03_zero_one_counter/zero_one_counter.sv
@@ -5,18 +5,13 @@ module zero_one_counter (
   output logic [31:0] zero_cnt,
   output logic [31:0] one_cnt
 );
-  logic [31:0] zero_cnt_reg, one_cnt_reg;
-
-  assign zero_cnt = zero_cnt_reg + (!cur_bit);
-  assign one_cnt = one_cnt_reg + cur_bit;
-
   always_ff @(posedge clk) begin
     if (rst) begin
-      zero_cnt_reg <= 0;
-      one_cnt_reg <= 0;
+      zero_cnt <= 0;
+      one_cnt <= 0;
     end else begin
-      zero_cnt_reg <= zero_cnt;
-      one_cnt_reg <= one_cnt;
+      zero_cnt <= zero_cnt + (!cur_bit);
+      one_cnt <= one_cnt + cur_bit;
     end
   end
 endmodule
