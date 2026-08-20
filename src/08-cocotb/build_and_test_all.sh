2a68d61330c7f58f14d7d8db0bb227b33b004c59
Timofeev Matvey <nomeow228@gmail.com>
2026-08-20 22:21:18 +0300

    fix: make colors visible in CI output

--- src/08-cocotb/build_and_test_all.sh
+++ src/08-cocotb/build_and_test_all.sh
@@ -5,35 +5,37 @@ MAKE_GENERAL_OPS="--silent"
 COL="\033[1;34m" # Bold blue
 COL_OFF="\033[0m"
 
+export COCOTB_ANSI_OUTPUT=1
+
 echo "----------------------------------------"
 echo "7 module test:"
 
 cd 01_merge_parallel_test
-echo -e "${COL}\nmerge_parallel module test...${COL_OFF}"
+printf "\n${COL}merge_parallel module test...${COL_OFF}"
 make $MAKE_GENERAL_OPS || error=1
 
 cd ../02_serial_adder_test
-echo -e "${COL}\nserial_1bit_adder module test...${COL_OFF}"
+printf "\n${COL}serial_1bit_adder module test...${COL_OFF}"
 make $MAKE_GENERAL_OPS || error=1
 
 cd ../03_pairwise_test
-echo -e "${COL}\npairwise module test...${COL_OFF}"
+printf "\n${COL}pairwise module test...${COL_OFF}"
 make $MAKE_GENERAL_OPS || error=1
 
 cd ../04_windowed_test
-echo -e "${COL}\nwindowed module test...${COL_OFF}"
+printf "\n${COL}windowed module test...${COL_OFF}"
 make $MAKE_GENERAL_OPS || error=1
 
 cd ../05_convolution_1d_test
-echo -e "${COL}\nconvolution_1d module test...${COL_OFF}"
-COCOTB_ANSI_OUTPUT=1 python3 -u convolution_1d_test.py 2>&1 | grep -v sorry
+printf "\n${COL}convolution_1d module test...${COL_OFF}"
+python3 -u convolution_1d_test.py 2>&1 | grep -v sorry
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
         error=1
 fi
 
 cd ../convolution_2d
-echo -e "${COL}\nconvolution_2d module test...${COL_OFF}"
-COCOTB_ANSI_OUTPUT=1 python3 -u convolution_2d_test.py 2>&1 | grep -v "sorry"
+printf "\n${COL}сonvolution_2d module test...${COL_OFF}"
+python3 -u convolution_2d_test.py 2>&1 | grep -v "sorry"
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
         error=1
 fi
